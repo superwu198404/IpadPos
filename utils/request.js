2b235d50d9120e23206041c30ae76354d55627ff
superwu198404 <18672326939@163.com>
2022-04-27 11:26:38 +0800

    D:\newipad\IpadPos\newPro\IpadPos\utils\request.js  请求里添加了  异常处理的模块

--- utils/request.js
+++ utils/request.js
@@ -12,7 +12,7 @@ const showToast = (title, icon = "none") => {
 
 /**请求接口
  * @method http
- * @param {String} url 接口地址1
+ * @param {String} url 接口地址
  * @param {Object} data 接口参数
  * @param {Object} option 接口配置信息，可选
  * @param {string} msg 加载提示信息，可选
@@ -146,6 +146,17 @@ const Post = function(urls, datas, msgs, option = {}, func) {
 	}
 };
 
+ Function retData(pm_code,pm_msg,pm_http)
+{
+  this.code  =	pm_code;
+  this.msg   =  pm_msg;
+  this.http  = pm_http
+	
+}
+
+
+
+
 //请求方法
 let httpFunc = function(pm_data) {
 	if (!pm_data.url) {
@@ -158,15 +169,10 @@ let httpFunc = function(pm_data) {
 	uni.showLoading({
 		title: pm_data.title || "加载中..."
 	});
-	let p_url; //判断是走业务后台还是走接口平台
-	if (pm_data.url_type && pm_data.url_type == 'jkpt') {
-		p_url = jkptUrl;
-	} else {
-		p_url = baseUrl
-	}
+
 	return new Promise(function(resolve, reject) {
 		uni.request({
-			url: p_url + pm_data.url,
+			url: baseUrl + pm_data.url,
 			method: pm_data.method || "POST",
 			header: {
 				'Content-Type': pm_data.method == 'GET' ?
@@ -175,10 +181,11 @@ let httpFunc = function(pm_data) {
 			data: pm_data.data,
 			success: (res) => {
 				uni.hideLoading();
-				if (res.statusCode == 200) {
+				if (res.statusCode == 200) 
+				{
 					return resolve(res.data);
 				} else {
-					return reject(res.errMsg);
+					return reject(  new retData(false,res.errMsg));
 				}
 			},
 			fail: (res) => {
@@ -187,43 +194,82 @@ let httpFunc = function(pm_data) {
 			}
 		})
 	}).catch(function(reason) {
-		if (catchfun)
-			catchfun(reason);
+		//if (catchfun)
+			//catchfun(reason);
 		console.log('异常捕捉catch:', reason);
 	});
 }
+
+
 
 //处理回调的地方 放外面
 let forPromise = function(func, pm_data) {
 	return new Promise(function(resolve, reject) {
 		// func(pm_data)
 		// return resolve(pm_data);
-		return resolve(func(pm_data));
+		try
+		{
+		  return resolve(func(pm_data));
+		}
+		catch(e)
+		{
+			return  reject( new retData(false,e.message) ); 
+		}	
 	})
 };
-//异步处理方法
+///作为异常和finally 的默认载体   
+let def  =function( pm_callback,pm_data)
+{
+	uni.hideLoading();
+	if( pm_callback )
+	{
+	   pm_callback(pm_data);
+	}
+}
+/// 
+///  res  =  { code:ture/false,msg:"消息",http:{url，title,method}，data:{}} 
+//异步处理方法3
 var asyncFunc = async function RequestDataArray2(pm_data, callbackfun, callbackfun2, callbackfun3, catchfun,
 	finallyfun) {
 	var callbacklist = [];
-
-	for (var i = 1; i <= 3; i++) {
-		if (arguments[i]) {
-			callbacklist.push(arguments[i]);
-		}
+	
+	for(var i= 1;i<=3;i++)
+	{
+	   if( arguments[i])
+	   {
+		   callbacklist.push(arguments[i]);
+	   }
 	}
 	let res = pm_data;
-	for (var i = 0; i < callbacklist.length; i++) {
+	for (var i = 0; i < callbacklist.length; i++) 
+	{
 		debugger;
-		if (res && res.http) {
+		if (res && res.http)
+		{
 			res = await httpFunc(res);
+			if(!res.code)
+			{
+			   def(catchfun,res);
+				break;
+			}
 		}
 		res = await forPromise(callbacklist[i], res)
+			if(!res.code)
+			{
+			   def(catchfun,res);
+				break;
+			}
 	}
+	//到这里 应该httpFunc 与  forPromise 都执行完成且状态改变 应该直接运行即可
+	if (finallyfun)  def(finallyfun,res);
+	/*
 	Promise.all([httpFunc, forPromise]).then(function(f_res) {
 		if (finallyfun)
 			finallyfun(f_res);
 	})
+	*/
 };
+
 
 export default {
 	http,
