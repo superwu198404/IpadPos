aeb1586c9c300f62e8ad44e607dafdceb83a784c
AloneInfinity <1425205619@qq.com>
2022-06-15 17:45:42 +0800

    电子券查询

--- api/Pay/Alipay.js
+++ api/Pay/Alipay.js
@@ -2,29 +2,49 @@ import Req from '@/utils/request.js';
 import rsa from '@/utils/rsa.js';
 
 var def_body = {
-	    "paytype": "",
-	    "method": "",
-	    "param": {}, 
-	    "data": {}
+	"paytype": "",
+	"method": "",
+	"param": {},
+	"data": {}
 };
 
 //请求处理入口
-const RequestHandle = (title, body = {...def_body},...callbacks) => {
+const RequestHandle = (title, body = {
+	...def_body
+}, ...callbacks) => {
 	Req.asyncFunc(...[{
 		http: true,
 		url: "PaymentAll/Handle",
 		title: title,
 		method: "POST",
 		data: {
-			paytype:"AliPay_ScanCode",
-			method:body.method,
-			param:body.param,
-			sign:rsa.rsaEncrypt(JSON.stringify(body.data))
+			paytype: "AliPay_ScanCode",
+			method: body.method,
+			param: body.param,
+			sign: rsa.rsaEncrypt(JSON.stringify(body.data))
 		}
-	},...callbacks]);
+	}, ...callbacks]);
 }
 
-const Payment = (title, apiName, type, body={},fun1,fun4) => {
+//请求处理入口(电子券查询)
+const ECouponQuery = (title, body = [], ...callbacks) => {
+	Req.asyncFunc(...[{
+		http: true,
+		url: "Hy/hy",
+		title: title,
+		method: "POST",
+		data: {
+			apiname: "TICKETQUERY",
+			appid: "keengee",
+			paramkey: "acc",
+			data: {
+				GT_IMPORT: body
+			}
+		}
+	}, ...callbacks]);
+}
+
+const Payment = (title, apiName, type, body = {}, fun1, fun4) => {
 	Req.asyncFunc({
 		http: true,
 		url: "/Payment/Payment",
@@ -35,7 +55,7 @@ const Payment = (title, apiName, type, body={},fun1,fun4) => {
 			type: type,
 			body: body
 		}
-	},fun1,null,null,fun4);
+	}, fun1, null, null, fun4);
 }
 
 export default {
