da2f1d5a6f98adfb9ccab4969330abbb5a3c2c5f
AloneInfinity <1425205619@qq.com>
2022-08-09 15:31:57 +0800

    提交

--- pages/Extract/Reserve/Reserve.vue
+++ pages/Extract/Reserve/Reserve.vue
@@ -8,10 +8,10 @@
 	<view class="boxs">
 		<view class="memberes">
 			<!-- v-if="Memberinfo" -->
-			<view class="meminfo">
+			<view v-if="views.Memberinfo" class="meminfo">
 				<view class="member">
 					<label>填写预定信息</label>
-					<button>×</button>
+					<button @click="views.Memberinfo = false">×</button>
 				</view>
 				<view class="middle">
 					<view class="restlist">
@@ -23,7 +23,7 @@
 						<label><text>配送方式：</text><input type="text" /></label>
 						<label><text>备注：</text><textarea></textarea></label>
 					</view>
-					<view class='rests' v-if="Newaddr" style="margin-bottom: 0; padding-bottom: 0;">
+					<view class='rests' style="margin-bottom: 0; padding-bottom: 0;">
 						<view class="h6"><text>新增地址</text></view>
 						<view class="restlist">
 							<label><text>收货人：</text><input type="text" /></label>
@@ -35,7 +35,7 @@
 										class="btn-sc">删除</button></view>
 							</view>
 						</view>
-	
+
 					</view>
 					<view class='rests'>
 						<view class="h6"><text>地址</text> <button @click="Newlys()">+ 新增地址</button></view>
@@ -55,7 +55,7 @@
 						<view class="more">显示全部地址<image src="../../../images/zhankaiqb-dt.png"></image>
 						</view>
 					</view>
-	
+
 					<view class="atlas">
 						<div class="map"></div>
 					</view>
@@ -65,49 +65,53 @@
 					<button class="btn">确 定</button>
 				</view>
 			</view>
-	
+
 			<view class="pop-r">
 				<view class="member">
 					<label>
 						<image class="touxiang" src="../../../images/touxiang.png"></image>
-						<label class="meminfo"><text>会员名称</text><text>13012341234</text></label>
+						<label
+							class="meminfo"><text>{{ details.main.CUSTMNAME || "-" }}</text><text>{{ details.main.CUSTMPHONE || "-" }}</text></label>
 					</label>
 					<text>清空</text>
 				</view>
-				<view class="h5"><text>单号：K200QTD00542205171540340</text></view>
+				<view class="edit-info">
+					<button @click="views.Memberinfo = true">+修改预订单信息</button>
+				</view>
+				<view class="h5"><text>单号：{{ details.main.BILL || "-" }}</text></view>
 				<view class="goods">
 					<!-- 商品循环 -->
-					<view class="prolist">
+					<view class="prolist" v-for="i in details.list">
 						<view class="h3">
 							<label>
-								<image src="../../../images/dx-mrxk.png" mode="widthFix"></image> 芝士绵绵绿豆糕
+								<image src="../../../images/dx-mrxk.png" mode="widthFix"></image> {{ i.SNAME || "-"  }}
 							</label>
-							<text>X2</text>
+							<text>X{{ i.QTY }}</text>
 						</view>
 						<view class="cods">
 							<view>
 								<label>
-									<image src="../../../images/dx-bm.png" mode="widthFix"></image>12345678
+									<image src="../../../images/dx-bm.png" mode="widthFix"></image>{{ i.SPID || "-"  }}
 								</label>
 								<label>
-									<image src="../../../images/dx-dw.png" mode="widthFix"></image>10个装
+									<image src="../../../images/dx-dw.png" mode="widthFix"></image>{{ i.UNIT || "-" }}
 								</label>
 							</view>
-							<text>￥56</text>
+							<text>￥{{ i.PRICE }}</text>
 						</view>
 					</view>
-	
+
 				</view>
-	
+
 				<view class="ul">
-					<view class="li"><text>总金额</text><text>￥567</text></view>
-					<view class="li"><text>件数</text><text>7</text></view>
-					<view class="li"><text>折扣</text><text>-￥5</text></view>
-					<view class="li"><text>应收金额</text><text>￥560</text></view>
+					<view class="li"><text>总金额</text><text>￥{{ details.main.ZNET }}</text></view>
+					<view class="li"><text>件数</text><text>{{ details.list.length }}</text></view>
+					<view class="li"><text>折扣</text><text>-￥{{ details.main.BILLDISC }}</text></view>
+					<view class="li"><text>应收金额</text><text>￥{{ details.main.DNET }}</text></view>
 				</view>
-	
+
 				<view class="confirm">
-					<button class="btn btn-qx">返 回</button>
+					<button class="btn btn-qx" @click="Close">返 回</button>
 					<button class="btn">确 认</button>
 				</view>
 			</view>
@@ -116,28 +120,85 @@
 </template>
 
 <script>
-	export default{
-		name:"Reserve",
-		props:{
-			show:Boolean,
-			data:Object
+	import _extract from '@/api/business/extract.js'
+	import util from '@/utils/util.js';
+	export default {
+		name: "Reserve",
+		props: {
+			show: Boolean,
+			bill: String
 		},
-		data(){
+		data() {
 			return {
-				Newaddr:false,
-				Statements:this.show
+				Newaddr: true,
+				Statements: this.show,
+				views: {
+					Memberinfo: false
+				},
+				details: {
+					list: [], //列表数据（后端查询的是一个商品集合）
+					main: {
+						BILL: "",
+						CUSTMNAME: "",
+						CUSTMPHONE: "",
+						DNET: "",
+						ZNET: "",
+						BILLDISC: ""
+					} //主单数据（商品集合中共有的部分，如买家的信息）
+				}
 			}
 		},
-		methods:{
+		methods: {
 			Newlys: function(e) {
 				this.Newaddr = true
 			},
+			GetOrderDetails: function(bill) { //查询获取订单商品详细信息
+				_extract.getReserveOrdersDetails({
+					khid: this.KHID,
+					bhlb: `(${util.getStorage("POSCS")?.find(i => i.POSCS === 'BHLBBM')?.POSCSNR || "109"})`,
+					bill
+				}, util.callBind(this, function(res) {
+					if (res.code) {
+						this.details.list = JSON.parse(res.data);
+						if (this.details.list.length > 0) {
+							this.details.main.CUSTMNAME = this.details.list[0].CUSTMNAME;
+							this.details.main.CUSTMPHONE = this.details.list[0].CUSTMPHONE;
+							this.details.main.DNET = this.details.list[0].DNET;
+							this.details.main.ZNET = this.details.list[0].ZNET;
+							this.details.main.BILLDISC = this.details.list[0].BILLDISC;
+							this.details.main.BILL = this.details.list[0].BILL;
+						}
+						console.log("详细信息：", res);
+					} else
+						util.simpleMsg(res.msg, true, res);
+				}))
+			},
+			Close: function() {
+				this.$emit("Close");
+			}
 		},
 		mounted() {
-			
+			this.GetOrderDetails(this.bill);
 		}
 	}
 </script>
 
 <style>
+	.edit-info > uni-button {
+	    flex: 0.9;
+		font-size: 12px;
+		margin-bottom: 4px;
+	}
+	.edit-info {
+		display: flex;
+		justify-content: center;
+	}
+	.pop-r {
+	    display: flex;
+	    flex-flow: column;
+	}
+	.goods {
+	    height: unset;
+	    flex: 0.98;
+	}
 </style>
