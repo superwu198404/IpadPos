1b6da0176fd099519ce91e87ff0718a28ddacb33
lytidus <751530927@qq.com>
2022-08-12 18:00:55 +0800

    促销一些方法

---
+++ pages/TestDemon/CxCount.vue
@@ -0,0 +1,65 @@
+<template>
+	<view class="body">
+		<view>
+			<view style="height: 50px;line-height: 50px;">请输入</view>
+			<view>
+				<input style="border:1px solid gray" type="text"  />
+			</view>
+		</view>
+		<button class="button" hover-class="hover" v-show="false">
+			测试
+		</button>
+		<button class="button" hover-class="hover">
+			按钮
+		</button>
+	</view>
+</template>
+<script>
+	import cxCount from '@/utils/cx/cxCount.js';
+	var app = getApp();
+	var that;
+
+	export default {
+		data() {
+			return {
+				ZKDatas: [],
+			}
+		},
+		methods: {
+			onLoad: function() {
+				that = this;
+				// let columns = ["name", "age", "sex"];
+				// let columns1 = ["spid", "sname", "price"];
+				// console.log("重组集合：", cxCount.CreateArr(columns));
+				// console.log("重组集合1：", cxCount.CreateArr(columns1));
+
+				cxCount.Cxdict();
+			},
+			CreateArr1: e => {
+				let arr = [],
+					obj = {};
+				e.map((r, i) => {
+					obj[r] = i
+				})
+				arr.push(obj);
+				return arr;
+			},
+
+		}
+	}
+</script>
+<style>
+	.button {
+		margin-top: 20px;
+		width: 90%;
+		background-color: #54bec2;
+		color: white;
+		border-radius: 98rpx;
+		background: bg_red;
+	}
+
+	/* 按下变颜色 */
+	.hover {
+		background: #dcdcdc;
+	}
+</style>
