280badf3b58e39e0508d92b918b226b47cb5d151
K-XG <2281381966@qq.com>
2022-08-24 19:32:16 +0800

    更正配置信息

--- utils/configInfo.js
+++ utils/configInfo.js
@@ -3,6 +3,7 @@
 // const baseUrl = 'http://192.168.0.201:44378/Api/'; //iis业务后台
 // const baseUrl = 'http://wgpay.zczyofficial.com:8001/Api/'; //华为云业务后台
 const baseUrl = 'http://58.19.103.220:8805/Api/'; //仟吉uat业务后台
+// const baseUrl = 'http://yjcxpw.natappfree.cc/Api/'; //仟吉uat业务后台
 const centerUrl = 'http://localhost:44377/Api/'; //初始化地址
 
 export default 
