ef3cd56c755feb51342221d2f803ee7c2bf33d1a
thisname9 <1273646007@qq.com>
2022-09-30 14:17:17 +0800

    1

--- pages/mainSale/MainSale.vue
+++ pages/mainSale/MainSale.vue
@@ -155,7 +155,8 @@
 						</label>
 					</view>
 					<view class="price">
-						<text class="jiage">￥{{mainSale.clikSpItem.PRICE}}</text><text v-if="mainSale.clikSpItem.ynAddPro" class="jiage">+加料总价{{mainSale.clikSpItem.NEWPRICE}}={{mainSale.clikSpItem.PRICE+mainSale.clikSpItem.NEWPRICE}}</text>
+						<text class="jiage">￥{{mainSale.clikSpItem.PRICE}}</text>
+						<text v-if="mainSale.clikSpItem.ynAddPro" class="jiage zongjia">+加料总价{{mainSale.clikSpItem.NEWPRICE}}={{mainSale.clikSpItem.PRICE+mainSale.clikSpItem.NEWPRICE}}</text>
 						<view>
 							<button @click="mainSale.chengedQty" data-qty="-1"
 								:disabled="mainSale.clikSpItem.ynAddPro">-</button>
@@ -165,7 +166,7 @@
 					</view>
 					<view class="tochoose">
 						<view v-for=" (sp, spinx) in mainSale.sale002" v-if="sp.BARCODE == mainSale.clikSpItem.SPID">
-						  <label><text>X{{sp.QTY}}</text>-<text>{{sp.UNIT}}</text>
+						  <label class="shux"><text>×{{sp.QTY}}</text>-<text>{{sp.UNIT}}</text>
 							  <text v-for="(sx08, sxindex) in mainSale.sale008" v-if="sp.NO==sx08.NO" >[{{sx08.ATTNAME}}{{sx08.QTY?("x"+sx08.QTY):""}}]</text>
 						  </label>
 							<label><text>￥{{sp.PRICE}}</text>
@@ -585,6 +586,8 @@
 	.popup {
 		position: relative;
 		padding-bottom: 140rpx;
+		max-height: 85%;
+		overflow: auto;
 	}
 
 	.popup .confirm {
@@ -594,6 +597,7 @@
 		width: 90%;
 		left: 5%;
 		text-align: center;
+		background-color: #fff;
 	}
 
 	.pop-rs .goods {
@@ -657,5 +661,8 @@
 	.bianji image {
 		width: 50rpx;
 		height: 50rpx;
+	}
+	.zongjia{
+		font-size: 28rpx;
 	}
 </style>
