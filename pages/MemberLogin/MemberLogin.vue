fd7af239189109d7240860111ecd49e00f56fd5f
K-XG <2281381966@qq.com>
2022-11-02 11:27:39 +0800

    1

--- pages/MemberLogin/MemberLogin.vue
+++ pages/MemberLogin/MemberLogin.vue
@@ -6,22 +6,23 @@
 	<view class="boxs">
 		<!-- <menu_page :menuIndex="0"></menu_page> -->
 		<!-- <view class="right"> -->
-			<!-- <menu_head></menu_head> -->
-			<!-- <Head></Head> -->
-			<view class="mem-bg">
-				<button class="skip" @click="ReBack()">×</button>
-				<image class="img-bg" src="../../images/hydl-bj.png" mode="widthFix"></image>
-				<view class="import">
-					<view class="h1">会员登录</view>
-					<text class="zim">Member login</text>
-					<label>
-						<image src="../../images/zhanghu.png" mode="widthFix"></image>
-						<input type="number" v-model="numbers" :adjust-position="false" focus="true"
-							:hold-keyboard="true" />
-					</label>
-					<button @click="HYLogin()">登 录</button>
-				</view>
+		<!-- <menu_head></menu_head> -->
+		<!-- <Head></Head> -->
+		<view class="mem-bg">
+			<button class="skip" @click="ReBack()">×</button>
+			<image class="img-bg" src="../../images/hydl-bj.png" mode="widthFix"></image>
+			<view class="import">
+				<view class="h1">会员登录</view>
+				<text class="zim">Member login</text>
+				<label>
+					<image src="../../images/zhanghu.png" mode="widthFix"></image>
+					<input type="number" v-model="numbers" :adjust-position="false" focus="true" :hold-keyboard="true"
+						@confirm="HYLogin" />
+					<!-- <input v-model="realBarcode" type="text" @keydown="handleKeyUp" /> -->
+				</label>
+				<button @click="HYLogin()">登 录</button>
 			</view>
+		</view>
 		<!-- </view> -->
 	</view>
 </template>
@@ -47,6 +48,8 @@
 				numbers: "",
 				brand: app.globalData.brand,
 				kquser: app.globalData.kquser,
+				realBarcode: "",
+				isScanningGun: false
 			}
 		},
 		methods: {
@@ -57,40 +60,54 @@
 			//登录事件
 			HYLogin: function() {
 				if (!that.numbers) {
-					util.simpleMsg("请输入手机号", true);
+					util.simpleMsg("请输入手机号，会员号", "none");
 					return;
 				}
-				let flag = _checker.checkMobile(that.numbers);
-				let obj = {
-					code: that.numbers,
-					type: flag ? 'Mobile' : 'ACCOUNT'
-				}
-				_member.QueryHyInfo("查询中...", {
-					brand: that.brand,
-					kquser: that.kquser,
-					data: obj
-				}, res => {
-					console.log("会员信息查询成功:", res);
-					if (res.code) {
-						let hyinfo = JSON.parse(res.data);
-						util.setStorage("hyinfo", hyinfo);
-						util.simpleMsg("登录成功");
-						uni.$emit("member-close", hyinfo);
-						// setTimeout(r => {
-						// 	uni.navigateBack({
-						// 		success: function() {
-						// 			eventChannel.emit('refreshHY', {
-						// 				data: hyinfo,
-						// 				msg: '会员登录页面返回'
-						// 			})
-						// 		}
-						// 	})
-						// }, 1000);
+				let store = util.getStorage("store");
+				if (that.numbers.length > 11) { //扫码操作
+					let obj = {
+						khid: store.KHID,
+						code: that.numbers
 					}
-				}, err => {
-					console.log("会员信息查询失败:", err);
-					util.simpleMsg(err.msg, "none");
-				});
+					_member.QueryHyInfoByCode(obj,
+						function(res) {
+							if (res.code) {
+								let hyinfo = JSON.parse(res.data);
+								util.setStorage("hyinfo", hyinfo);
+								util.simpleMsg("登录成功");
+								uni.$emit("member-close", hyinfo);
+							} else {
+								util.simpleMsg(res.msg, "none");
+							}
+						}, err => {
+							console.log("会员信息查询失败:", err);
+							util.simpleMsg(err.msg, "none");
+						});
+				} else {
+					let flag = _checker.checkMobile(that.numbers);
+					let obj = {
+						code: that.numbers,
+						type: flag ? 'Mobile' : 'ACCOUNT'
+					}
+					_member.QueryHyInfo("查询中...", {
+						brand: that.brand,
+						kquser: that.kquser,
+						data: obj
+					}, res => {
+						console.log("会员信息查询成功:", res);
+						if (res.code) {
+							let hyinfo = JSON.parse(res.data);
+							util.setStorage("hyinfo", hyinfo);
+							util.simpleMsg("登录成功");
+							uni.$emit("member-close", hyinfo);
+						} else {
+							util.simpleMsg(res.msg, "none");
+						}
+					}, err => {
+						console.log("会员信息查询失败:", err);
+						util.simpleMsg(err.msg, "none");
+					});
+				}
 			},
 			//返回
 			ReBack: function() {
@@ -103,7 +120,60 @@
 						})
 					}
 				});
-			}
+			},
+			// 处理keyup事件
+			handleKeyUp(e) {
+				console.log("按键事件:", e);
+				var shiftKey = e.shiftKey //为true则按了shiftKey
+				let keyCode = e.code
+				var key = e.key //其他按键key
+				let array = ["Q", "W", "E", "R", "T", "Y", "U",
+					"I", "O", "P", "A", "S", "D", "F", "G", "H", "J",
+					"K", "L", "Z", "X", "C", "V", "B", "N", "M",
+					"1", "2", "3", "4", "5", "6", "7", "8", "9", "0"
+				]
+				// 这种情况下 基本上是 中文输入法 才有出现
+				if (key == "Process") {
+					for (const a of array) {
+						// 如果匹配到是英文
+						if (keyCode == "Key" + a) {
+							// 判断大小写
+							if (shiftKey) {
+								this.realBarcode += a
+								this.isScanningGun = true
+							} else {
+								this.realBarcode += (a.toLowerCase())
+								this.isScanningGun = true
+							}
+						} else
+							// 如果匹配到是数字
+							if (keyCode == "Digit" + a) {
+								this.realBarcode += String(a)
+								this.isScanningGun = true
+							}
+					}
+					if (keyCode == "Enter") {
+						console.log("")
+						// this.clickBarCode()
+					}
+				} else
+					// 这是英文状态下 直接判断输入的英文在没在上面大写字母中
+					if (array.includes(key.toUpperCase())) {
+						this.realBarcode += key
+					} else
+						// 这是英文状态下 直接判断输入的小写英文在没在上面大写字母中
+						if (array.includes(key.toUpperCase())) {
+							this.realBarcode += key
+						} else
+							// 这是数字 直接判断输入的数字在没在上面数字中
+							if (array.includes(key)) {
+								this.realBarcode += String(key)
+							}
+				else if (keyCode == "Enter") {
+					// 监听到enter触发了，执行后续事件
+					// this.clickBarCode()
+				}
+			},
 		},
 		created() {
 			that = this;
@@ -117,7 +187,7 @@
 		height: 90%;
 		padding: 2%;
 		position: relative;
-		margin:5% auto;
+		margin: 5% auto;
 	}
 
 	.mem-bg .img-bg {
@@ -139,25 +209,28 @@
 		background: rgba(0, 107, 68, 0.3);
 		text-align: center;
 	}
-	.mem-bg .import .h1{
+
+	.mem-bg .import .h1 {
 		font-size: 80rpx;
 		font-weight: 700;
 		line-height: 100rpx;
 		text-align: center;
-		margin-top:-50rpx;
+		margin-top: -50rpx;
 	}
-	.mem-bg .import .zim{
+
+	.mem-bg .import .zim {
 		display: block;
 		font-size: 50rpx;
 		line-height: 160rpx;
 	}
+
 	.mem-bg .import label {
 		display: flex;
 		background-color: #fff;
 		width: 76%;
 		align-items: center;
 		padding: 0 2%;
-		margin:40rpx auto;
+		margin: 40rpx auto;
 		height: 84rpx;
 		line-height: 84rpx;
 	}
@@ -193,6 +266,6 @@
 		line-height: 80rpx;
 		width: 80rpx;
 		font-size: 40rpx;
-		padding:0;
+		padding: 0;
 	}
 </style>
