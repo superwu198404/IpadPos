879d2f99ebd479ba0bb9e157b634a21cadef1d83
K-XG <2281381966@qq.com>
2022-11-10 18:48:26 +0800

    折扣精度修复

--- api/common.js
+++ api/common.js
@@ -694,6 +694,18 @@ var ResetAuthCode = function(e) {
 	return code;
 }
 
+//四舍六入  返回number类型
+var newFixed = function(num, decimalPlaces) {
+	var d = decimalPlaces || 0;
+	var m = Math.pow(10, d);
+	var n = +(d ? num * m : num).toFixed(8); // Avoid rounding errors
+	var i = Math.floor(n),
+		f = n - i;
+	var e = 1e-8; // Allow for rounding errors in f
+	var r = (f > 0.5 - e && f < 0.5 + e) ?
+		((i % 2 == 0) ? i : i + 1) : Math.round(n);
+	return d ? r / m : r;
+}
 export default {
 	InitData,
 	CreateBill,
@@ -718,5 +730,6 @@ export default {
 	GetPOSCS_Local,
 	GetTXFILE,
 	GetPayOrRefund,
-	ResetAuthCode
+	ResetAuthCode,
+	newFixed
 }
