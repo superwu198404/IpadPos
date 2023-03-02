7b0e22e93d06823880f4e5dd99b9aac7e8b945e4
K-XG <2281381966@qq.com>
2023-03-02 16:16:13 +0800

    1

--- api/hy/MemberInterfaces.js
+++ api/hy/MemberInterfaces.js
@@ -357,7 +357,7 @@ var GetTLCard = function(store, func) {
 			return;
 		}
 		console.log("参数信息：", config);
-		Req.asyncFuncOne(paymentAll.CreateData("TL", "请刷卡...",
+		Req.asyncFuncOne(paymentAll.CreateData("TL", "请进行刷卡...",
 			"ReadCard", { //这里固定写成通联的原因是因为，刷卡接口写在MIS的Payment里在，且因为使用刷卡机要包装一系列参数，而MIS内有方法处理，其他类里没有
 				store_id: config.KEY,
 				terminalCode: config.NOTE,
