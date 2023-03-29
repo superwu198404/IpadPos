db3b49339757a121a1c0b2696413e4bdda2a95fb
Crimson <1111111111@qq.com>
2023-03-29 09:15:38 +0800

    提交

--- pages/CouponSale/CouponSale.vue
+++ pages/CouponSale/CouponSale.vue
@@ -40,6 +40,7 @@
 						</view>
 						<!-- 刷卡后显示卡列表 -->
 						<view class="cardlist">
+							<NoData v-if="get_main_sale6.length==0"></NoData>
 							<view class="ulli" v-for="(sale6_main,index) in get_main_sale6">
 								<view class="touch-list list-touch" @click="touch_list($event,sale6_main.sale006)"
 									:data-style="get_text_style(sale6_main.sale006)" :data-index="index"
