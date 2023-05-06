5081b16bf6430ccda0780438a7dc555d1d0bed02
K-XG <2281381966@qq.com>
2023-05-06 15:22:14 +0800

    卡激活接口传值调整

--- utils/configInfo.js
+++ utils/configInfo.js
@@ -1,4 +1,3 @@
-//const baseUrl = 'http://192.168.0.28:44377/Api/'; //本地业务地址
 // const baseUrl = 'http://192.168.0.28:44377/Api/'; //本地业务地址
 // const baseUrl = 'http://192.168.0.201:44378/Api/'; //iis业务后台
 // const baseUrl = 'http://wgpay.zczyofficial.com:8001/Api/'; //华为云业务后台
