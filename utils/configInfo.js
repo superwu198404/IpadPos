bdba4ce0cd12aacc0c5dc7ffc7c3f66c7c763a98
superwu198404 <18672326939@163.com>
2023-09-28 17:36:53 +0800

    11

--- utils/configInfo.js
+++ utils/configInfo.js
@@ -3,7 +3,7 @@
 // const baseUrl = 'http://wgpay.zczyofficial.com:8001/Api/'; //华为云业务后台
 const baseUrl = 'http://183.94.2.127:8805/Api/'; //仟吉uat业务后台
 // const baseUrl = 'http://192.168.0.28:44377/Api/'; //本地业务后台
- // const baseUrl = 'http://pad.kengee.com.cn:8018/Api/'; //仟吉生产业务后台
+//  const baseUrl = 'http://pad.kengee.com.cn:8018/Api/'; //仟吉生产业务后台
  
 
 export default {
